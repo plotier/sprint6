437cceaa3c1ba0c84d051f84886fdbd1130addb4
plotier <plotier15@gmail.com>
2022-01-04 12:02:34 +0100

    first commit

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Escena = ()=>{
+return "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
+
+}
+
+export default Escena;
